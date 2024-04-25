deec64d4d9d9a56e614e0d2eef07ee9927c8f76b
Alex Morland <Alex.Morland@ottogroup.com>
2024-04-25 15:33:17 +0200

    refactoring add second test case

--- src/test/kotlin/com/almland/springbootbatch/module2/example3/BillingJobReadFlatFileWriteDbReadDbWriteFlatFileConfigurationTest.kt
+++ src/test/kotlin/com/almland/springbootbatch/module2/example3/BillingJobReadFlatFileWriteDbReadDbWriteFlatFileConfigurationTest.kt
@@ -3,15 +3,16 @@ package com.almland.springbootbatch.module2.example3
 import com.almland.springbootbatch.utils.PostgreSqlTestConfiguration
 import java.nio.file.Files
 import java.nio.file.Paths
-import org.junit.jupiter.api.AfterEach
+import java.util.stream.Stream
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.batch.core.ExitStatus
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.test.JobLauncherTestUtils
-import org.springframework.batch.test.JobRepositoryTestUtils
 import org.springframework.batch.test.context.SpringBatchTest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -24,44 +25,72 @@ import org.springframework.test.jdbc.JdbcTestUtils
 @SpringBatchTest // registers the JobLauncherTestUtils and JobRepositoryTestUtils as Spring beans
 internal class BillingJobReadFlatFileWriteDbReadDbWriteFlatFileConfigurationTest(
     @Autowired private val jdbcTemplate: JdbcTemplate,
-    @Autowired private val jobLauncherTestUtils: JobLauncherTestUtils,
-    @Autowired private val jobRepositoryTestUtils: JobRepositoryTestUtils
+    @Autowired private val jobLauncherTestUtils: JobLauncherTestUtils
 ) {
 
     companion object {
         private const val DB_TABLE_NAME = "BILLING_DATA"
+
+        @JvmStatic
+        fun getBillingJobTestData(): Stream<Arguments> = Stream.of(
+            Arguments.arguments(
+                "src/test/resources/billing/copy/billing-2023-01-test.csv",
+                "src/test/resources/billing/billing-2023-01-test.csv",
+                "src/test/resources/billing/report/billing-2023-01-test-report.csv",
+                2023,
+                1,
+                781
+            ),
+            Arguments.arguments(
+                "src/test/resources/billing/copy/billing-2023-02-test.csv",
+                "src/test/resources/billing/billing-2023-02-test.csv",
+                "src/test/resources/billing/report/billing-2023-02-test-report.csv",
+                2023,
+                2,
+                781
+            )
+        )
     }
 
     @BeforeEach
     fun setUp() {
-        jobRepositoryTestUtils.removeJobExecutions()
         JdbcTestUtils.deleteFromTables(jdbcTemplate, DB_TABLE_NAME)
     }
 
-    @AfterEach
-    fun tearDown() {
-        Files.deleteIfExists(Paths.get("src/test/resources/", "billing/report/billing-2023-report.csv"))
-    }
-
-    @Test
-    fun `billing job execution`() {
+    @ParameterizedTest
+    @MethodSource("getBillingJobTestData")
+    fun `billing job execution file`(
+        copyFileTarget: String,
+        inputFile: String,
+        reportFileTarget: String,
+        year: Int,
+        month: Int,
+        expectedDataCount: Long
+    ) {
         val jobParameters = JobParametersBuilder()
-            .addString("input.file.copy.target", "src/test/resources/billing/copy/billing-2023-01-test.csv")
-            .addString("input.file", "src/test/resources/billing/billing-2023-01-test.csv")
-            .addString("output.file", "src/test/resources/billing/report/billing-2023-01-test-report.csv")
-            .addJobParameter("data.year", 2023, Int::class.java)
-            .addJobParameter("data.month", 1, Int::class.java)
+            .addString("input.file.copy.target", copyFileTarget)
+            .addString("input.file", inputFile)
+            .addString("output.file", reportFileTarget)
+            .addJobParameter("data.year", year, Int::class.java)
+            .addJobParameter("data.month", month, Int::class.java)
             .toJobParameters()
 
         val jobExecution = jobLauncherTestUtils.launchJob(jobParameters)
 
         assertEquals(ExitStatus.COMPLETED, jobExecution.exitStatus)
-        assertTrue(Files.exists(Paths.get("src/test/resources/", "billing/copy/billing-2023-01-test.csv")))
+        assertTrue(
+            Files.exists(
+                Paths.get(
+                    copyFileTarget.substringBefore("billing"),
+                    copyFileTarget.substringAfter("resources/")
+                )
+            )
+        )
         assertEquals(1000, JdbcTestUtils.countRowsInTable(jdbcTemplate, DB_TABLE_NAME))
 
-        with(Paths.get("src/test/resources/", "billing/report/billing-2023-01-test-report.csv")) {
+        with(Paths.get(reportFileTarget.substringBefore("billing"), reportFileTarget.substringAfter("resources/"))) {
             assertTrue(Files.exists(this))
-            assertEquals(781, Files.lines(this).count())
+            assertEquals(expectedDataCount, Files.lines(this).count())
         }
     }
 }
